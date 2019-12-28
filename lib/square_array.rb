e2f98aca4d4a5ff2555dbaa4a348315abd477449
Corey Hodge <contact@coreyhodge.net>
2019-12-28 01:51:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  newArray = []
+  while array[counter] do
+    numToSquare = array[counter]
+    numToSquare *= numToSquare
+    array.push(numToSquare)
+    counter += 1
+  end
 end
